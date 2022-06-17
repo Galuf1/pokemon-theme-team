1ed7e896ea1176fcbb12a0a0cdeb9f55d7508daf
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-17 15:57:28 +0200

    cleaning a bit

--- index.js
+++ index.js
@@ -21,18 +21,21 @@ const getPokeBois = async () => {
     
 
 const getTeam = async() => {
-    let typess = document.getElementById("typepok").className
-    let list = []
-    response3 = await axios.get(`https://pokeapi.co/api/v2/type/${typess}`)
-    console.log(response3)
-    let totalType = response3.data.pokemon.length
-    for (let i = 0; i < 5; i++){
-        let random = Math.floor(Math.random()*totalType)
-        let randompoke = response3.data.pokemon[random].pokemon.url
-        response4 = await axios.get(randompoke)
-        let img = document.createElement("img")
-        img.src = response4.data.sprites.front_default
-        document.getElementById("sprites").appendChild(img)
+    try {
+        let type = document.getElementById("typepok").className
+        response3 = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
+        let totalType = response3.data.pokemon.length
+        for (let i = 0; i < 5; i++){
+            let random = Math.floor(Math.random()*totalType)
+            let randompoke = response3.data.pokemon[random].pokemon.url
+            response4 = await axios.get(randompoke)
+            let img = document.createElement("img")
+            img.src = response4.data.sprites.front_default
+            document.getElementById("sprites").appendChild(img)
+        }
     }
-
-}
+    catch(e) {
+        console.log('NOPE, NICE TRY')
+        console.log(e)
+        }
+    }
