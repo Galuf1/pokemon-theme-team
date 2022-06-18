b79d20ee1f823d6073f1cbdf77b4e5ce02a8448a
Galuf1 <daniel.pizarro.farias@gmail.com>
2022-06-18 15:24:50 +0200

    switches and globals  added

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+let typestarter;
+let starter = false
+let team = false
 const getPokeBois = async () => {
     try {
-        let response = await axios.get('https://pokeapi.co/api/v2/pokemon-species/?limit=0')
-        const pokeNum = response.data.count
-        const randomNum = Math.floor(Math.random() * pokeNum)
-        response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
-        let pokeType = response.data.types[0].type['name']
-        let firstSprite = response.data.sprites.front_default
-        let img = document.createElement('img')
-        img.src = firstSprite
-        img.className = pokeType
-        img.id = "typepok"
-        document.getElementById("sprites").appendChild(img);
-
+        if (!starter) {
+            let response = await axios.get('https://pokeapi.co/api/v2/pokemon-species/?limit=0')
+            const pokeNum = response.data.count
+            const randomNum = Math.floor(Math.random() * pokeNum)
+            response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${randomNum}`)
+            let pokeType = response.data.types[0].type['name']
+            typestarter = pokeType
+            let firstSprite = response.data.sprites.front_default
+            let img = document.createElement('img')
+            img.src = firstSprite
+            document.getElementById("sprites").appendChild(img);
+            starter = true
+        } else {
+            alert("starter already created")
+        }    
     }
     catch(e){
             console.log('NOPE, NICE TRY')
@@ -22,18 +28,22 @@ const getPokeBois = async () => {
 
 const getTeam = async() => {
     try {
-        let type = document.getElementById("typepok").className
-        response3 = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
-        let totalType = response3.data.pokemon.length
-        for (let i = 0; i < 5; i++){
-            let random = Math.floor(Math.random()*totalType)
-            let randompoke = response3.data.pokemon[random].pokemon.url
-            response4 = await axios.get(randompoke)
-            let img = document.createElement("img")
-            img.src = response4.data.sprites.front_default
-            document.getElementById("sprites").appendChild(img)
+        if (starter && !team) {
+            team = true
+            response3 = await axios.get(`https://pokeapi.co/api/v2/type/${typestarter}`)
+            let totalType = response3.data.pokemon.length
+            for (let i = 0; i < 5; i++){
+                let random = Math.floor(Math.random()*totalType)
+                let randompoke = response3.data.pokemon[random].pokemon.url
+                response4 = await axios.get(randompoke)
+                let img = document.createElement("img")
+                img.src = response4.data.sprites.front_default
+                document.getElementById("sprites").appendChild(img)
+            }
+        } else {
+            alert("team already created")
         }
-    }
+    } 
     catch(e) {
         console.log('NOPE, NICE TRY')
         console.log(e)
